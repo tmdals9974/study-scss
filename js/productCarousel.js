808c65040f92f8b9bf3463c7afc1ce3694db42c9
dev_sm <tmdals9974@naver.com>
2022-09-27 23:29:01 +0900

    feat: tiny-slider.js 적용 및 productCarousel 생성

---
+++ js/productCarousel.js
@@ -0,0 +1,12 @@
+const productCarouse = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
