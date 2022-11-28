d8dd11cd2b44f2a9fc2345f1e27b54fdcb417048
dev_sm <tmdals9974@naver.com>
2022-11-28 23:15:26 +0900

    rename: rename carousel script

--- js/carousels.js
+++ js/carousels.js
@@ -16,6 +16,8 @@ const userGallery = tns({
   gutter: 4,
   edgePadding: 16,
   controls: false,
+  navContainer: '.user-gallery .thumbnail-list',
+  navAsThumbnails: true,
   loop: false,
   arrowKeys: true,
   mouseDrag: true,
